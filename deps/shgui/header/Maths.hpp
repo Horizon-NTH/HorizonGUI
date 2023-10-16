5acd42aba0cde03553ff644a83e74053737888cf
Horizon <bjr.asd@gmail.com>
2023-10-16 17:52:19 +0200

    fix

--- deps/shgui/header/Maths.hpp
+++ deps/shgui/header/Maths.hpp
@@ -270,15 +270,10 @@ namespace hgui
 			Point(U x, V y) noexcept;
 			template<typename U>
 			Point(const Point<U>& point) noexcept;
-			template<typename U>
-			Point(const kernel::Vector<U, 2>& point) noexcept;
 
 			Point<T>& operator=(const Point<T>& point) noexcept;
-			Point<T>& operator=(const kernel::Vector<T, 2>& point) noexcept;
 			template<typename U>
 			Point<T>& operator=(const Point<U>& point) noexcept;
-			template<typename U>
-			Point<T>& operator=(const kernel::Vector<U, 2>& point) noexcept;
 			Point<T>& operator=(const glm::vec2& point) noexcept;
 
 			friend std::ostream& operator<<(std::ostream& stream, const hgui::kernel::Point<T>& point)
@@ -317,15 +312,10 @@ namespace hgui
 			Size(U width, V height) noexcept;
 			template<typename U>
 			Size(const Size<U>& size) noexcept;
-			template<typename U>
-			Size(const Vector<U, 2>& size) noexcept;
 
 			Size<T>& operator=(const Size<T>& size) noexcept;
-			Size<T>& operator=(const Vector<T, 2>& size) noexcept;
 			template<typename U>
 			Size<T>& operator=(const Size<U>& size) noexcept;
-			template<typename U>
-			Size<T>& operator=(const Vector<U, 2>& size) noexcept;
 			Size<T>& operator=(const glm::vec2& size) noexcept;
 
 			friend std::ostream& operator<<(std::ostream& stream, const hgui::kernel::Size<T>& size)
@@ -472,6 +462,13 @@ inline hgui::kernel::GLSLvec2<T>::GLSLvec2(U x, V y) noexcept :
 template<typename T>
 inline hgui::kernel::GLSLvec2<T>::GLSLvec2(const GLSLvec2<T>& vector) noexcept :
 	Vector<T, 2>({ vector.x, vector.y }), x((*this)[0]), y((*this)[1])
+{
+}
+
+template<typename T>
+template<typename U>
+inline hgui::kernel::GLSLvec2<T>::GLSLvec2(const GLSLvec2<U>& vector) noexcept :
+	Vector<T, 2>({ static_cast<T>(vector.x), static_cast<T>(vector.y) }), x((*this)[0]), y((*this)[1])
 {
 }
 
@@ -823,13 +820,6 @@ inline hgui::kernel::Point<T>::Point(const Point<U>& point) noexcept :
 }
 
 template<typename T>
-template<typename U>
-inline hgui::kernel::Point<T>::Point(const Vector<U, 2>& point) noexcept :
-	Vector<T, 2>({ static_cast<T>(point[0]), static_cast<T>(point[1])}), x((*this)[0]), y((*this)[1])
-{
-}
-
-template<typename T>
 inline hgui::kernel::Point<T>::Point(const glm::vec2& point) noexcept :
 	Vector<T, 2>({ static_cast<T>(point.x), static_cast<T>(point.y) }), x((*this)[0]), y((*this)[1])
 {
@@ -844,28 +834,11 @@ inline hgui::kernel::Point<T>& hgui::kernel::Point<T>::operator=(const Point<T>&
 }
 
 template<typename T>
-inline hgui::kernel::Point<T>& hgui::kernel::Point<T>::operator=(const Vector<T, 2>& point) noexcept
-{
-	x = point[0];
-	y = point[1];
-	return *this;
-}
-
-template<typename T>
 template<typename U>
 inline hgui::kernel::Point<T>& hgui::kernel::Point<T>::operator=(const Point<U>& point) noexcept
 {
 	x = static_cast<T>(point.x);
 	y = static_cast<T>(point.y);
-	return *this;
-}
-
-template<typename T>
-template<typename U>
-inline hgui::kernel::Point<T>& hgui::kernel::Point<T>::operator=(const Vector<U, 2>& point) noexcept
-{
-	x = static_cast<T>(point[0]);
-	y = static_cast<T>(point[1]);
 	return *this;
 }
 
@@ -980,13 +953,6 @@ inline hgui::kernel::Size<T>::Size(const Size<U>& size) noexcept :
 }
 
 template<typename T>
-template<typename U>
-inline hgui::kernel::Size<T>::Size(const Vector<U, 2>& size) noexcept :
-	Vector<T, 2>({ static_cast<T>(size[0]), static_cast<T>(size[1])}), width((*this)[0]), height((*this)[1])
-{
-}
-
-template<typename T>
 inline hgui::kernel::Size<T>::Size(const glm::vec2& size) noexcept :
 	Vector<T, 2>({ static_cast<T>(size.x), static_cast<T>(size.y) }), width((*this)[0]), height((*this)[1])
 {
@@ -1001,14 +967,6 @@ inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const Size<T>& si
 }
 
 template<typename T>
-inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const Vector<T, 2>& size) noexcept
-{
-	width = size[0];
-	height = size[1];
-	return *this;
-}
-
-template<typename T>
 template<typename U>
 inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const Size<U>& size) noexcept
 {
@@ -1016,16 +974,6 @@ inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const Size<U>& si
 	height = static_cast<T>(size.height);
 	return *this;
 }
-
-template<typename T>
-template<typename U>
-inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const Vector<U, 2>& size) noexcept
-{
-	width = static_cast<T>(size[0]);
-	height = static_cast<T>(size[1]);
-	return *this;
-}
-
 
 template<typename T>
 inline hgui::kernel::Size<T>& hgui::kernel::Size<T>::operator=(const glm::vec2& size) noexcept
