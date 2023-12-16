1817255ece59dad7b042aed3f249d15f5c666236
Horizon <bjr.asd@gmail.com>
2023-12-16 16:58:54 +0100

    fix

--- src/shaders/generate_shaders.cmake
+++ src/shaders/generate_shaders.cmake
@@ -1,5 +1,5 @@
 # Vertex
-file(GLOB_RECURSE GLSL_VERTEX_SOURCES ${CMAKE_SOURCE_DIR}/src/shaders/vertex/*.glsl)
+file(GLOB_RECURSE GLSL_VERTEX_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/shaders/vertex/*.glsl)
 foreach(SHADER_FILE ${GLSL_VERTEX_SOURCES})
     file(STRINGS ${SHADER_FILE} SHADER_CONTENT)
     get_filename_component(FILE_NAME ${SHADER_FILE} NAME_WE)
@@ -9,7 +9,7 @@ foreach(SHADER_FILE ${GLSL_VERTEX_SOURCES})
 endforeach()
 
 # Fragment
-file(GLOB_RECURSE GLSL_FRAGMENT_SOURCES ${CMAKE_SOURCE_DIR}/src/shaders/fragment/*.glsl)
+file(GLOB_RECURSE GLSL_FRAGMENT_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/shaders/fragment/*.glsl)
 foreach(SHADER_FILE ${GLSL_FRAGMENT_SOURCES})
     file(STRINGS ${SHADER_FILE} SHADER_CONTENT)
     get_filename_component(FILE_NAME ${SHADER_FILE} NAME_WE)
@@ -18,4 +18,4 @@ foreach(SHADER_FILE ${GLSL_FRAGMENT_SOURCES})
     set(FRAGMENT_${FILE_NAME} ${SHADER_CONTENT})
 endforeach()
 
-configure_file(${CMAKE_SOURCE_DIR}/src/shaders/GLSL.h.in ${CMAKE_SOURCE_DIR}/include/hgui/header/GLSL.h @ONLY)
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/src/shaders/GLSL.h.in ${CMAKE_CURRENT_SOURCE_DIR}/include/hgui/header/GLSL.h @ONLY)
